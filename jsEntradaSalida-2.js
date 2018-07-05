c4c08e54abdcfb02e257982af9778fcb84f7c8ca
fedecampoos <40859084+fedecampoos@users.noreply.github.com>
2018-07-05 11:24:52 -0300

    primer comit
    estos son los primeros 4 ejercicio  de la primera clase

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,17 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+	var nombre; 
+	//nombre= "federico";
+	//alert(nombre);
+	nombre = prompt("ingrese su nombre");
+	alert("su nombre es :"+ nombre);
+	/* var person = prompt("Please enter your name", "Harry Potter");
+    /*ESTO ES UN COMENTARIO DE BLOQE 
+    ESTO LO SACAMOS DE W3SCHOOLS */
+
+
+
+}
+
